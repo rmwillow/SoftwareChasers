e5e2019ba03f79f8de6c56622b736883c29a384c
rmwillow <52016382+rmwillow@users.noreply.github.com>
2021-07-06 10:50:22 -0500

    get manager title from manager

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,2 +1,9 @@
 const Manager = require('../lib/Manager.js');
 const Employee = require("../lib/Employee");
+
+test("getJobTitle() should return \"Manager\"", () => {
+    const testValue = "Manager";
+    const newManager = new Manager("tempVal", 1, "test@gmail.com", testValue);
+    expect(newManager.getJobTitle()).toBe(testValue);
+  });
+  
