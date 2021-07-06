eebec8e69c8801b438e34ed65298376b26edca18
rmwillow <52016382+rmwillow@users.noreply.github.com>
2021-07-06 17:30:21 -0500

    engineer prompt

--- index.js
+++ index.js
@@ -13,7 +13,36 @@ let roleArray = [];
 
 
 //create prompt user function with inquirer & question choices
-
+function promptUser(answers) {
+    return inquirer.prompt([
+        {
+            type: "list",
+            name: "role",
+            message: "what is your role?",
+            choices: ["Engineer", "Intern", "Manager"]
+        },
+    ]).then(function (res) {
+        // should use switch case instead of if/else starting here
+        console.log(res)
+        if (res.role === "Engineer") {
+            inquirer.prompt([
+                {
+                    name: "name",
+                    message: "What is your name?",
+                    type: "input"
+                },
+                {
+                    name: "github",
+                    type: "input",
+                    message: "What is your github Username?"
+                },
+                {
+                    name: "email",
+                    type: "input",
+                    message: "What is your email?"
+                }
+           
+            }
 //use switch case for if the user chooses engineer, intern or manager
 //create user prompt for each role
 
