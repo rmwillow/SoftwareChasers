2037325490cceb417554466bfab6f2020955b4da
rmwillow <52016382+rmwillow@users.noreply.github.com>
2021-07-06 09:49:01 -0500

    employee tests failed correctly

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,1 +1,50 @@
 const Employee = require('../lib/Employee.js');
+
+
+test("Can create new object from employee class", () => {
+  const employTest = new Employee();
+  //using type of to get type of and tobe to set it the type we want (jest documentation)
+  expect(typeof(employTest)).toBe("object");
+});
+
+test("Can set employee name to alice", () => {
+  const name = "Alice";
+  const employTest = new Employee(name);
+  expect(employTest.name).toBe(name);
+});
+
+test("Can set employee ID to testValue", () => {
+  const testValue = 100;
+  const employTest = new Employee("Foo", testValue);
+  expect(employTest.id).toBe(testValue);
+});
+
+test("Can set email to be testValue", () => {
+  const testValue = "test@test.com";
+  const employTest = new Employee("Foo", 1, testValue);
+  expect(employTest.email).toBe(testValue);
+});
+
+test("Can get name from getName()", () => {
+  const testValue = "Alice";
+  const employTest = new Employee(testValue);
+  expect(employTest.getName()).toBe(testValue);
+});
+
+test("Can get id from getId()", () => {
+  const testValue = 100;
+  const employTest = new Employee("Foo", testValue);
+  expect(employTest.getId()).toBe(testValue);
+});
+
+test("Can get email from getEmail()", () => {
+  const testValue = "test@test.com";
+  const employTest = new Employee("Foo", 1, testValue);
+  expect(employTest.getEmail()).toBe(testValue);
+});
+
+test("getRole() should return \"Employee\"", () => {
+  const testValue = "Employee";
+  const employTest = new Employee("Alice", 1, "test@test.com");
+  expect(employTest.getRole()).toBe(testValue);
+});
