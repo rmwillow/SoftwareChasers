0e9f6a5b27bff878680e5babf45604f83e49e7de
rmwillow <52016382+rmwillow@users.noreply.github.com>
2021-07-06 17:21:21 -0500

    imports and empty role array

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 //import inquirer
+const inquirer = require("inquirer");
 //import jest
+const jest = require("jest");
 //require Engineer, intern, manager
+const Engineer = require("./lib/engineer.js");
+const Intern = require("./lib/intern.js");
+const Manager = require("./lib/manager.js");
 //create a role id of 0
+let roleId = 0;
 //create empty array
+let roleArray = [];
 
 
 //create prompt user function with inquirer & question choices
