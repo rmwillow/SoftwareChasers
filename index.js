1da26a29bdcd368ec5c2befa98b3d73cf3947bf8
rmwillow <52016382+rmwillow@users.noreply.github.com>
2021-07-06 17:18:37 -0500

    sudo code for index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+//import inquirer
+//import jest
+//require Engineer, intern, manager
+//create a role id of 0
+//create empty array
+
+
+//create prompt user function with inquirer & question choices
+
+//use switch case for if the user chooses engineer, intern or manager
+//create user prompt for each role
+
+
+//push answers to team array for each job resolve
+
+//generate HTML 
+
+
