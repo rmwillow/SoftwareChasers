08483bb8f3e3074a8e9124415eafa578522b5eac
rmwillow <52016382+rmwillow@users.noreply.github.com>
2021-07-14 17:03:54 -0500

    intern and manager  tests reconstructed

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,20 +1,19 @@
-const Intern = require('../lib/Intern.js');
+const Intern = require("../lib/Intern");
 
-test("Can set school object from constructor", () => {
-    const testValue = "UT";
-    const tempIntern = new Intern("tempVal", 1, "test@gmail.com", testValue);
-    expect(tempIntern.school).toBe(testValue);
-  });
+test("Can set school via constructor", () => {
+  const testValue = "UCLA";
+  const e = new Intern("Foo", 1, "test@test.com", testValue);
+  expect(e.school).toBe(testValue);
+});
 
-  test("getRole() should return \"Intern\"", () => {
-    const testValue = "Intern";
-    const tempIntern = new Intern("tempVal", 1, "test@gmail.com", "UT");
-    expect(tempIntern.getRole()).toBe(testValue);
-  });
-  
+test("getRole() should return \"Intern\"", () => {
+  const testValue = "Intern";
+  const e = new Intern("Foo", 1, "test@test.com", "UCLA");
+  expect(e.getRole()).toBe(testValue);
+});
 
-  test("Can get school from getSchoolName()", () => {
-    const testValue = "UT";
-    const tempIntern = new Intern("tempVal", 1, "test@gmail.com", testValue);
-    expect(tempIntern.getSchoolName()).toBe(testValue);
-  });
+test("Can get school via getSchool()", () => {
+  const testValue = "UCLA";
+  const e = new Intern("Foo", 1, "test@test.com", testValue);
+  expect(e.getSchool()).toBe(testValue);
+});
